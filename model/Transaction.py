f0a7c227981142deac2752ee11f76a00f7c98f85
LeeSeokmin <dltjrals13@naver.com>
2024-12-26 13:59:32 +0900

    거래 내역 모델 생성

---
+++ model/Transaction.py
@@ -0,0 +1,66 @@
+from hashlib import sha256
+import rsa
+
+
+def apply_sha256(input):
+    try:
+        digest = sha256()
+        digest.update(input.encode('utf-8'))
+        hash_bytes = digest.digest()
+        hex_string = ''.join(f'{b:02x}' for b in hash_bytes)
+        return hex_string
+    except Exception as e:
+        print(e)
+        raise RuntimeError(e)
+
+
+class Transaction:
+    def __init__(self, sender_account_num: int, recipient_account_num: int, amount: int, previous_hash: str):
+        self.sender = sender_account_num
+        self.recipient = recipient_account_num
+        self.amount = amount
+        self.hash: str = self.compute_hash()
+        self.previous_hash = previous_hash
+        self.signature: [bytes] = None
+
+    def compute_hash(self):
+        calculatedHash = apply_sha256(
+            str(self.sender) + str(self.recipient) + str(self.amount)
+        )
+
+        return calculatedHash
+
+    def generateSignature(self, private_key):
+        data: str = str(self.sender) + str(self.recipient) + self.hash
+        try:
+            signature = rsa.sign(data.encode(), private_key, 'SHA-256')
+            self.signature = signature
+        except Exception as e:
+            print(e)
+
+    def verifySignature(self, public_key) -> bool:
+        data: str = str(self.sender) + str(self.recipient) + self.hash
+        try:
+            rsa.verify(data.encode('utf-8'), self.signature, public_key)
+            return True
+        except (ValueError, TypeError) as e:
+            print(e)
+        return False
+
+    def getSenderAccountNum(self) -> int:
+        return self.sender
+
+    def getRecipientAccountNum(self) -> int:
+        return self.recipient
+
+    def getHash(self) -> str:
+        return self.hash
+
+    def getPreviousHash(self) -> str:
+        return self.previous_hash
+
+    def setHash(self, hash: str):
+        self.hash = hash
+
+    def setPreviousHash(self, previous_hash: str):
+        self.previous_hash = previous_hash
