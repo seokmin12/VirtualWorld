4e8f615c45b3d3b18aec2fd536ddfa120e26c1c0
LeeSeokmin <dltjrals13@naver.com>
2025-01-01 17:21:58 +0900

    땅 주인 변경 함수 추가

--- model/Invest/Land.py
+++ model/Invest/Land.py
@@ -20,6 +20,7 @@ class Land:
         self.zoning = zoning
         self.buildable = buildable
         self.base_price_per_sqm = base_price_per_sqm
+        self.owner = None
         self.price = self.calculate_price()
 
     def calculate_price(self):
@@ -30,9 +31,16 @@ class Land:
         self.zoning = new_zoning
         print(f"{self.name} zoning  changed to {self.zoning}.")
 
+    def change_owner(self, user):
+        if self.owner == user:
+            print(f"{user.name} already own {self.name}.")
+            return
+        self.owner = user
+        print(f"{self.name}'s owner changed to {user.name}.")
+
     def __str__(self):
         return (
-            f"Land: {self.name}, Location: {self.location}, Area: {self.area} sqm, "
+            f"Land: {self.name}, Area: {self.area} sqm, "
             f"Zoning: {self.zoning}, Buildable: {'Yes' if self.buildable else 'No'}, "
             f"Base Price per sqm: ${self.base_price_per_sqm:.2f}, Total Price: ${self.price:.2f}, "
         )
